57ae16f36a6da72ee865bb1f6812311ed8bd3526
Vasil Vasilev <vvasilev@vgvasilev-macbook-pro.local>
2025-04-14 11:50:28 +0300

    Add profile image upload test and fix profile verification

---
+++ src/test/java/tests/EditProfileTests.java
@@ -0,0 +1,54 @@
+package tests;
+
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.EditProfilePage;
+import pages.HomePage;
+import pages.LoginPage;
+
+import java.time.Duration;
+
+public class EditProfileTests extends BaseTest {
+
+    @Test
+    public void testChangeProfilePicture() {
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+
+        HomePage home = new HomePage(driver);
+        home.clickLogin();
+
+        LoginPage login = new LoginPage(driver);
+        login.login("marmot1", "Marmot123");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(home.profileLink));
+
+        // go to profile page
+        driver.get("http://training.skillo-bg.com:4300/users/9435");
+
+        EditProfilePage profile = new EditProfilePage(driver);
+
+        // wait to load the profile
+        wait.until(ExpectedConditions.visibilityOf(profile.profileUsername));
+
+        // Hover over the upload profile pic
+        Actions actions = new Actions(driver);
+        actions.moveToElement(profile.profileImage).perform();
+
+        // click when the camera is shown
+        wait.until(ExpectedConditions.visibilityOf(profile.editProfilePictureDiv));
+        profile.editProfilePictureDiv.click();
+
+        // uploading the new picture
+        String imagePath = System.getProperty("user.dir") + "/src/test/resources/myProfilePic.png";
+        profile.uploadInput.sendKeys(imagePath);
+
+        // wait to see the new pic
+        wait.until(ExpectedConditions.visibilityOf(profile.profileImage));
+
+        Assert.assertTrue(profile.profileImage.isDisplayed(), "Profile image should be visible after upload.");
+    }
+}
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -13,22 +13,22 @@ public class LoginTests extends BaseTest {
 
     @Test
     public void testSuccessfulLogin() {
-        // Отвори началната страница
+        // open homepage
         driver.get("http://training.skillo-bg.com:4300/posts/all");
 
-        // Създай обект на началната страница
+        // create new objec
         HomePage home = new HomePage(driver);
         home.clickLogin();
 
-        // Създай обект на login страницата и логни
+        // login with user
         LoginPage login = new LoginPage(driver);
         login.login("marmot1", "Marmot123");
 
-        // Изчакай появата на линка към профила
+        // wait to see the profile link
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOf(home.profileLink));
 
-        // Провери, че линкът към профила се вижда
+        // check if the profile link is visible
         Assert.assertTrue(home.profileLink.isDisplayed(), "Профил линкът не се вижда!");
     }
 }
